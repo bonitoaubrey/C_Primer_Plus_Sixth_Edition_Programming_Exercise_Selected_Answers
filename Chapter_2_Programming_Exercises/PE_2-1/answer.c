45f97e8a75477b81ec9963370255bbb38f41e5cc
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-11-20 17:58:54 +0300

    add programming exercise 2-1

---
+++ Chapter_2_Programming_Exercises/PE_2-1/answer.c
@@ -0,0 +1,12 @@
+/* Programming Exercise 2-1 */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Gustav Mahler\n");
+    printf("Gustav\nMahler\n");
+    printf("Gustav ");
+    printf("Mahler\n");
+
+    return 0;
+}
