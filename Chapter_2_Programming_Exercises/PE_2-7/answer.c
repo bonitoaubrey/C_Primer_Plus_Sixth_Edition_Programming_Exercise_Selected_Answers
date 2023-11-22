046123d5655e70890a971aa5dc49f8f53bdcfd61
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-11-23 00:19:00 +0300

    add programming exercise 2-7

---
+++ Chapter_2_Programming_Exercises/PE_2-7/answer.c
@@ -0,0 +1,23 @@
+/* Programming Exercise 2-7 */ 
+
+#include <stdio.h>
+
+void print_smile(void) {
+    printf("Smile!");
+}
+
+int main(void) {
+    print_smile();
+    print_smile();
+    print_smile();
+    printf("\n");
+
+    print_smile();
+    print_smile();
+    printf("\n");
+
+    print_smile();
+    printf("\n");
+    
+    return 0;
+}
