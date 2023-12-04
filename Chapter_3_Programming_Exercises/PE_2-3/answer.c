38f58854dadfef268ccf901f0fcced3e222b5ef4
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-12-04 21:58:54 +0300

    add programming exercise 3-3

---
+++ Chapter_3_Programming_Exercises/PE_2-3/answer.c
@@ -0,0 +1,10 @@
+/* Programming Exercise 3-3 */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("%c", 7);
+    printf("Startled by the sudden sound, Sally shouted,\n");
+    printf("\"By the Great Pumpkin, what was that!\"\n");
+    return 0;
+}
