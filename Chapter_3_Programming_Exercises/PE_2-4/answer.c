f862bf8bb87e8c6611ec8aa3f07d4a161a15afea
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-12-06 13:08:27 +0300

    add programming exercise 3-4

---
+++ Chapter_3_Programming_Exercises/PE_2-4/answer.c
@@ -0,0 +1,16 @@
+/* Programming Exercise 3-4 */
+
+#include <stdio.h>
+
+int main(void) {
+    float num;
+    
+    printf("Enter a floating-point value: ");
+    scanf("%f", &num);
+
+    printf("fixed-point notation: %f\n", num);
+    printf("exponential notation: %e\n", num);
+    printf("p notation: %a\n", num);
+
+    return 0;
+}
