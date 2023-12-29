69865f6bf5d1a284a2766b4744c7688c064ef638
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-12-29 13:27:33 +0300

    change programming exercise 3-5

--- Chapter_3_Programming_Exercises/PE_3-5/answer.c
+++ Chapter_3_Programming_Exercises/PE_3-5/answer.c
@@ -2,14 +2,14 @@
 
 #include <stdio.h>
 
-const float SECONDS_IN_YEAR = 3.156e10;
+const float SECONDS_IN_YEAR = 3.156e7;
 
 int main(void) {
     float age;
     printf("Enter your age in years: ");
     scanf("%f", &age);
-    printf("Your age in seconds: %.3e", 
-           age * SECONDS_IN_YEAR);
+    printf("Your age in seconds: %.3e",
+        age * SECONDS_IN_YEAR);
 
     return 0;
 }
