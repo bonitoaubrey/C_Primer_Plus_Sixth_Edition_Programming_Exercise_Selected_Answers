3a789080fd4a6bed43d9732bcb38eacd6bc738e6
Bonito Aubrey <bonitoaubrey@gmail.com>
2024-01-21 20:56:01 +0300

    change programming exersice 3-8

--- Chapter_3_Programming_Exercises/PE_3-8/answer.c
+++ Chapter_3_Programming_Exercises/PE_3-8/answer.c
@@ -2,25 +2,28 @@
 
 #include <stdio.h>
 
+const int CUPS_IN_PINT = 2;
+const int OUNCES_IN_CUP = 8;
+const int TABLESPOONS_IN_ONCE = 2;
+const int TEASPOONS_IN_TABLESPOONS = 3;
+
 int main(void) {
     float cups;
-    int pins;
+    int pints;
     int ounces;
     int tablespoons;
     int teaspoons;
 
-    printf("Enter a volume in cups: \n");
+    printf("Enter a volume in cups: ");
     scanf("%f", &cups);
-    teaspoons = cups * 48;
-    printf("The equivalent volumes: ");
-    pins = teaspoons / 96;
-    printf("%d pin(s) ", pins);
-    ounces = teaspoons % 96 / 6;
-    printf("%d ounce(s) ", ounces);
-    tablespoons = teaspoons % 96 % 6 / 3;
-    teaspoons = teaspoons % 3;
-    printf("%d tablespoon(s) ", tablespoons);
-    printf("%d teaspoon(s)", teaspoons);
-    printf("\n");
+    pints = cups / CUPS_IN_PINT;
+    ounces = (int)(cups * OUNCES_IN_CUP) % (OUNCES_IN_CUP * CUPS_IN_PINT);
+    tablespoons = (int)(cups * OUNCES_IN_CUP * TABLESPOONS_IN_ONCE) % TABLESPOONS_IN_ONCE;
+    teaspoons = (int)(cups * OUNCES_IN_CUP * TABLESPOONS_IN_ONCE * TEASPOONS_IN_TABLESPOONS) % TEASPOONS_IN_TABLESPOONS;
+
+
+    printf("The equivalent volumes: %d pint(s), %d ounce(s), %d tablespoon(s) and %d tespoon(s)\n", 
+             pints, ounces, tablespoons, teaspoons);
+
     return 0;
 }
