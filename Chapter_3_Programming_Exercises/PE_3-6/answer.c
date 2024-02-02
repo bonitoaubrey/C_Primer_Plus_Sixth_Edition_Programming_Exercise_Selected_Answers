45e686588361d1cfc455ba4ffeebb43a1f9cc5dd
Bonito Aubrey <bonitoaubrey@gmail.com>
2024-02-02 20:34:48 +0300

    add programming exercise 3-6

---
+++ Chapter_3_Programming_Exercises/PE_3-6/answer.c
@@ -0,0 +1,17 @@
+/* Programming Exercise 3-6 */
+
+#include <stdio.h>
+
+#define MOLECULE_OF_WATER_MASS 3e-23
+#define QUART_OF_WATER_MASS 950
+
+int main(void) {
+    double water_quarts;
+    printf("Enter an amount of water in quarts: ");
+    scanf("%lf", &water_quarts);
+
+    printf("The number of water molecules in that amount: %e",
+           water_quarts * QUART_OF_WATER_MASS / MOLECULE_OF_WATER_MASS);
+
+    return 0;
+}
