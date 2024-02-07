eed3dd8663ecb013b1a3ad7cc3f60a16797a7dee
Bonito Aubrey <bonitoaubrey@gmail.com>
2024-02-07 21:00:18 +0300

    change programming exercise 4-4

--- Chapter_4_Programming_Exercises/PE_4-4/answer.c
+++ Chapter_4_Programming_Exercises/PE_4-4/answer.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 int main(void) {
-    char name[20];
     float height;
+    char name[40];
 
-    printf("Write your name: ");
+    printf("Enter your name: ");
     scanf("%s", name);
-    printf("Write your height in centimeters: ");
+    printf("Enter your height in centimeters: ");
     scanf("%f", &height);
-    printf("%s, you are %.2f meters tall.\n", name, height / 100);
+    printf("%s, you are %.2f meters tall\n", name, height / 100);
 
     return 0;
 }
