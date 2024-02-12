2ddffcd7d454be2dcf68eb43458dfae7dbe3dbd2
Bonito Aubrey <bonitoaubrey@gmail.com>
2024-02-12 17:13:04 +0300

    add programming exercise 6-11

---
+++ Chapter_6_Programming_Exercises/PE_6-11/answer.c
@@ -0,0 +1,19 @@
+/* Programming Exercise 6-11 */
+
+#include <stdio.h>
+
+int main(void) {
+    int array[11];
+
+    printf("Enter eight integers: ");
+    
+    for (int i = 0; i < 8; i++)
+        scanf("%d", &array[i]);
+
+    for (int i = 7; i >= 0; i--)
+        printf("%4d", array[i]);
+
+    printf("\n");
+
+    return 0;
+}
