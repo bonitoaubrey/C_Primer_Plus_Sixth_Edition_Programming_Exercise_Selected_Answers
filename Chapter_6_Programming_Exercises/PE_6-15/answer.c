c9c1db99d52dd42b1a2cc73c4a6904a40f2f7f3b
Bonito Aubrey <bonitoaubrey@gmail.com>
2024-02-18 11:28:29 +0300

    change programming exercise 6-15

--- Chapter_6_Programming_Exercises/PE_6-15/answer.c
+++ Chapter_6_Programming_Exercises/PE_6-15/answer.c
@@ -6,9 +6,13 @@
 int main(void) {
     char str[255];
     size_t index;
+    int i = 0;
 
-    printf("Enter some text: ");
-    scanf("%s", str);
+    printf("Enter some text:\n");
+    scanf("%c", &str[i]);
+    while (str[i++] != '\n')
+        scanf("%c", &str[i]);
+    str[i] = '\0';
 
     for (index = strlen(str); index-- ;)
         printf("%c", str[index]);
