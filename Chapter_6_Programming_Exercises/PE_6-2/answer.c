9f90f0bce12387ba3224fae0a0bc615116be6985
Bonito Aubrey <bonitoaubrey@gmail.com>
2024-02-24 09:07:35 +0300

    change programming exercise 6-2

--- Chapter_6_Programming_Exercises/PE_6-2/answer.c
+++ Chapter_6_Programming_Exercises/PE_6-2/answer.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 int main(void) {
-    for (int i = 1; i <= 5; i++) {
-        for (int j = 0; j < i; j++)
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j <= i; j++)
             printf("$");
         printf("\n");
     }
