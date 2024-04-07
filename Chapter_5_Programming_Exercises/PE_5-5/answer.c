e350cdacb2c0317a87809f2cf5f62a1cd4a1ed34
Bonito Aubrey <bonitoaubrey@gmail.com>
2024-04-07 09:20:10 +0300

    add porgramming exercise 5-5

---
+++ Chapter_5_Programming_Exercises/PE_5-5/answer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int value;
+    int sum = 0;
+
+    printf("How far the calculation shoud procced?\n");
+    scanf("%d", &value);
+    for (int i = 1; value--; i++)
+        sum = sum + i;
+    printf("sum = %d\n", sum);
+    
+    return 0;
+}
