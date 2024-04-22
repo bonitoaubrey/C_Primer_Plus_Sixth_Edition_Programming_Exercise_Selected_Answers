84b1d483750cc66006f0bb60437545299f2bd302
Bonito Aubrey <bonitoaubrey@gmail.com>
2024-04-22 11:17:45 +0300

    change programming exercise 6-16

--- Chapter_6_Programming_Exercises/PE_6-16/answer.c
+++ Chapter_6_Programming_Exercises/PE_6-16/answer.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 
 int main(void) {
-    float Daphne = 100;
-    float Deirdre = 100;
+    float Daphne_investment = 100;
+    float Deirdre_investment = 100;
     int year;
 
-    for (year = 0; Daphne >= Deirdre; year++) {
-        Daphne += 10;
-        Deirdre += Deirdre * 5 / 100;
+    for (year = 0; Daphne_investment >= Deirdre_investment; year++) {
+        Daphne_investment += 10;
+        Deirdre_investment += Deirdre_investment * 5 / 100;
     };
     
-    printf("Year: %d   Daphne: %.2f   Deirdre: %.2f\n", year, Daphne, Deirdre);
+    printf("Year: %d   Daphne: %.2f   Deirdre: %.2f\n",
+           year, Daphne_investment, Deirdre_investment);
 
     return 0;
 }
